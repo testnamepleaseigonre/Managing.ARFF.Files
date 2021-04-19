cc0a4495cf1bb9a4738264f6f0d0e8d1abbb6994
Valdemar Subotkovski <valdemar.subotkovski@gmail.com>
2021-04-19 15:27:00 +0300

    Initial commit
    Created GUI, first version of code.

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Managing.ARFF.Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.catalogPathTextBox = new System.Windows.Forms.TextBox();
+            this.chooseCatalogButton = new System.Windows.Forms.Button();
+            this.checkedListBoxAll = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.combineButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // catalogPathTextBox
+            // 
+            this.catalogPathTextBox.Location = new System.Drawing.Point(14, 14);
+            this.catalogPathTextBox.Name = "catalogPathTextBox";
+            this.catalogPathTextBox.ReadOnly = true;
+            this.catalogPathTextBox.Size = new System.Drawing.Size(388, 20);
+            this.catalogPathTextBox.TabIndex = 0;
+            // 
+            // chooseCatalogButton
+            // 
+            this.chooseCatalogButton.Location = new System.Drawing.Point(200, 40);
+            this.chooseCatalogButton.Name = "chooseCatalogButton";
+            this.chooseCatalogButton.Size = new System.Drawing.Size(94, 23);
+            this.chooseCatalogButton.TabIndex = 1;
+            this.chooseCatalogButton.Text = "Choose folder";
+            this.chooseCatalogButton.UseVisualStyleBackColor = true;
+            this.chooseCatalogButton.Click += new System.EventHandler(this.chooseCatalogButton_Click);
+            // 
+            // checkedListBoxAll
+            // 
+            this.checkedListBoxAll.CheckOnClick = true;
+            this.checkedListBoxAll.FormattingEnabled = true;
+            this.checkedListBoxAll.Location = new System.Drawing.Point(14, 74);
+            this.checkedListBoxAll.Name = "checkedListBoxAll";
+            this.checkedListBoxAll.Size = new System.Drawing.Size(388, 214);
+            this.checkedListBoxAll.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(300, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Get all attributes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 294);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "All";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(95, 294);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "None";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // combineButton
+            // 
+            this.combineButton.Location = new System.Drawing.Point(327, 294);
+            this.combineButton.Name = "combineButton";
+            this.combineButton.Size = new System.Drawing.Size(75, 23);
+            this.combineButton.TabIndex = 11;
+            this.combineButton.Text = "Combine";
+            this.combineButton.UseVisualStyleBackColor = true;
+            this.combineButton.Click += new System.EventHandler(this.combineButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(408, 74);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(576, 214);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(895, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Write to .arff";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 342);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.combineButton);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBoxAll);
+            this.Controls.Add(this.chooseCatalogButton);
+            this.Controls.Add(this.catalogPathTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox catalogPathTextBox;
+        private System.Windows.Forms.Button chooseCatalogButton;
+        private System.Windows.Forms.CheckedListBox checkedListBoxAll;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button combineButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Managing.ARFF.Files
+{
+    public partial class Form1 : Form
+    {
+        private List<string> listOfAllAttributeNames;
+        private List<string> dataReadyToWrite;
+        private List<string> attributesReadyToWrite;
+        private string relation;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listOfAllAttributeNames = new List<string>();
+            catalogPathTextBox.Text = "C:\\Users\\valde\\Desktop\\arff-test";
+        }
+
+        private void chooseCatalogButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                catalogPathTextBox.Text = dialog.SelectedPath;
+                listOfAllAttributeNames = new List<string>();
+                dataReadyToWrite = new List<string>();
+                attributesReadyToWrite = new List<string>();
+                richTextBox1.Text = null;
+                checkedListBoxAll.Items.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            try
+            {
+                richTextBox1.Text = null;
+                dataReadyToWrite = new List<string>();
+                attributesReadyToWrite = new List<string>();
+                listOfAllAttributeNames = new List<string>();
+                checkedListBoxAll.Items.Clear();
+                getAllAttributes(catalogPathTextBox.Text);
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            button1.Enabled = true;
+        }
+
+        private void getAllAttributes(string folderPath)
+        {
+            relation = null;
+            string line;
+            bool fileIsUsable = false;
+            string[] files = Directory.GetFiles(folderPath, "*.arff", SearchOption.TopDirectoryOnly);
+            foreach (string file in files)
+            {
+                try
+                {
+                    if (Path.GetExtension(file) == ".arff")
+                    {
+                        StreamReader ongoingFile = new StreamReader(file);
+                        if(relation == null)
+                            relation = ongoingFile.ReadLine();
+                        if (relation.Contains("@relation"))
+                        {
+                            while ((line = ongoingFile.ReadLine()) != null)
+                            {
+                                if (line.Contains("@attribute") == true)
+                                {
+                                    listOfAllAttributeNames.Add(line);
+                                }
+                                if (line.Contains("@data") == true)
+                                {
+                                    fileIsUsable = true;
+                                }
+                            }
+                            if (fileIsUsable == true)
+                            {
+                                foreach (string attr in listOfAllAttributeNames)
+                                {
+                                    checkedListBoxAll.Items.Add(attr);
+                                }
+                                Console.WriteLine($"Found attributes: {listOfAllAttributeNames.Count}");
+                                //Console.WriteLine($"Found [.arff] files: {files.Length}");
+                                ongoingFile.Close();
+                                break;
+                            }
+                            else
+                            {
+                                throw new Exception($"File skipped [{Path.GetFileName(file)}]: \"@data\" tag not found!");
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception($"File skipped[{Path.GetFileName(file)}]: \"@realation\" tag not found!");
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception($"File skipped[{Path.GetFileName(file)}]: extention is not \".arff\"!");
+                    }
+                }
+                catch(Exception exc)
+                {
+                    relation = null;
+                    listOfAllAttributeNames.Clear();
+                    Console.WriteLine(exc.Message);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            setAllOrNoneChecked(true);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            setAllOrNoneChecked(false);
+        }
+
+        private void setAllOrNoneChecked(bool choice)
+        {
+            if (checkedListBoxAll.Items.Count != 0)
+            {
+                for (var i = 0; i < checkedListBoxAll.Items.Count; i++)
+                {
+                    checkedListBoxAll.SetItemChecked(i, choice);
+                }
+            }
+            else
+            {
+                if(choice == true)
+                    MessageBox.Show("Nothing to check!");
+                else
+                    MessageBox.Show("Nothing to uncheck!");
+            }
+        }
+
+        private void combineButton_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            chooseCatalogButton.Enabled = false;
+            button2.Enabled = false;
+            checkedListBoxAll.Enabled = false;
+            if (checkedListBoxAll.Items.Count != 0)
+            {
+                if(checkedListBoxAll.CheckedItems.Count != 0)
+                {
+                    try
+                    {
+                        dataReadyToWrite = new List<string>();
+                        attributesReadyToWrite = new List<string>();
+                        richTextBox1.Text = null;
+                        combineFunc(getListToCombine(), catalogPathTextBox.Text);
+                    }
+                    catch(Exception exc)
+                    {
+                        MessageBox.Show(exc.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Check at least one item to combine!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nothing to combine!");
+            }
+            checkedListBoxAll.Enabled = true;
+            button2.Enabled = true;
+            button1.Enabled = true;
+            chooseCatalogButton.Enabled = true;
+        }
+
+        private List<string> setCheckedItemsToList()
+        {
+            List<string> temp = new List<string>(); 
+            foreach (var attr in checkedListBoxAll.CheckedItems)
+            {
+                temp.Add(attr.ToString());
+            }
+            return temp;
+        }
+
+        private void combineFunc(List<string> toCombine, string folderPath)
+        {
+            attributesReadyToWrite = setCheckedItemsToList();
+            string allData;
+            string[] seperatedData = null;
+            string line;
+            bool fileIsUsable = false;
+            chooseCatalogButton.Enabled = false;
+            combineButton.Enabled = false;
+            foreach(string file in Directory.GetFiles(folderPath, "*.arff", SearchOption.TopDirectoryOnly))
+            {
+                try
+                {
+                    if (Path.GetExtension(file) == ".arff")
+                    {
+                        StreamReader ongoingFile = new StreamReader(file);
+                        if (ongoingFile.ReadLine() == relation)
+                        {
+                            while ((line = ongoingFile.ReadLine()) != null)
+                            {
+                                if (line.Contains("@data") == true)
+                                {
+                                    ongoingFile.ReadLine();
+                                    allData = ongoingFile.ReadLine();
+                                    seperatedData = allData.Split(',');
+                                    if (allData != "" && !string.IsNullOrEmpty(allData) && seperatedData.Length == listOfAllAttributeNames.Count)
+                                    {
+                                        fileIsUsable = true;
+                                    }
+                                }
+                            }
+                            if(fileIsUsable == true)
+                            {
+                                string oneFileData = null;
+                                foreach (string attr in listOfAllAttributeNames)
+                                {
+                                    foreach (string temp in attributesReadyToWrite)
+                                    {
+                                        if (temp == attr)
+                                        {
+                                            if (oneFileData != null)
+                                                oneFileData += ("," + seperatedData[listOfAllAttributeNames.IndexOf(attr)]);
+                                            else
+                                                oneFileData += seperatedData[listOfAllAttributeNames.IndexOf(attr)];
+                                        }
+                                    }
+                                }
+                                dataReadyToWrite.Add(oneFileData);
+                                richTextBox1.Text += $"{oneFileData}\n";
+                                //Console.WriteLine(oneFileData);
+                            }
+                            else
+                            {
+                                throw new Exception($"File skipped [{Path.GetFileName(file)}]: \"@data\" tag not found or data is corrupted!");
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception($"File skipped [{Path.GetFileName(file)}]: \"@realation\" tag not found!");
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception($"File skipped [{Path.GetFileName(file)}]: extention is not \".arff\"!");
+                    }
+                }
+                catch(Exception exc)
+                {
+                    fileIsUsable = false;
+                    Console.WriteLine(exc.Message);
+                }
+            }
+            chooseCatalogButton.Enabled = true;
+            combineButton.Enabled = true;
+        }
+
+        private List<string> getListToCombine()
+        {
+            List<string> final = new List<string>();
+            foreach(var attr in checkedListBoxAll.CheckedItems)
+            {
+                final.Add(attr.ToString());
+            }
+            return final;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataReadyToWrite.Count != 0 && attributesReadyToWrite.Count != 0)
+            {
+                List<string> lines = new List<string>();
+                lines.Add($"{relation}\n");
+                foreach(string attr in attributesReadyToWrite)
+                {
+                    lines.Add(attr);
+                }
+                lines.Add($"\n@data\n");
+                foreach(string data in dataReadyToWrite)
+                {
+                    lines.Add(data);
+                }                string docPath = "C:\\Users\\valde\\Desktop";
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "combined.arff")))
+                {
+                    foreach (string line in lines)
+                        outputFile.WriteLine(line);
+                }
+                MessageBox.Show("Data succesfully saved to file!");
+            }
+            else
+            {
+                MessageBox.Show("No data to write!");
+            }
+        }
+    }
+}
